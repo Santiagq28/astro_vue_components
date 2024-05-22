b778c6bb3c06ddc405d945b71bebed93e494b3bf
Santiago Guevara <saguevaraq@ufpso.edu.co>
2024-05-22 00:16:57 -0500

    Ejercicios 2

---
+++ src/components/vue/Parking.vue
@@ -0,0 +1,36 @@
+<script setup>
+    import { ref } from 'vue';
+    import Space from '../vue/Space.vue';
+    let total = ref(0);
+
+    let parqueadero = ref([0,0,0,0,0,0]);
+
+    function Parquear(precio, pos){
+        if(parqueadero.value[pos] == 0){
+            total.value += precio;
+            console.log(total.value);
+            parqueadero.value[pos] = 1;
+        }
+        else{
+            parqueadero.value[pos] = 0;
+        }
+    }
+
+</script>
+
+<template>
+    <div class="grid text-center justify-center">
+        <div class="flex gap-10">
+            <Space @click="Parquear(1000,0)" vehicule="Carro" :price="1000"/>
+            <Space @click="Parquear(1000,1)" vehicule="Carro" :price="1000" />
+            <Space @click="Parquear(1000,2)" vehicule="Carro" :price="1000" />
+        </div>
+        <div class="flex gap-10 mt-5">
+            <Space @click="Parquear(500,3)" vehicule="Moto" :price="500" />
+            <Space @click="Parquear(500,4)" vehicule="Moto" :price="500" />
+            <Space @click="Parquear(500,5)" vehicule="Moto" :price="500" />
+        </div>
+        <div class="text-center p-4 bg-zinc-500 rounded mt-4 text-white font-bold"><h6>{{ total }}</h6></div>
+    </div>
+    
+</template>
---
+++ src/components/vue/Space.vue
@@ -0,0 +1,39 @@
+<script setup>
+    import { ref } from 'vue';
+    import { defineProps } from 'vue';
+
+    let props = defineProps({
+        vehicule: String,
+        estado: String,
+        price: Number
+    })
+
+    let estado = ref('Disponible');
+    let libre = ref(true);
+    function Parquear(){
+        if(estado.value == 'Disponible'){
+            estado.value = 'Ocupado';
+            libre.value = false;
+        }else{
+            estado.value = 'Disponible';
+            libre.value = true;
+        }
+        
+
+    }
+
+</script>
+
+<template>
+    <button @click="Parquear" 
+    
+    class="p-4  text-white font-bold font-serif rounded w-[120px] text-center"
+    :class="{'bg-red-600 hover:bg-red-400': estado === 'Ocupado',
+        'bg-green-500 hover:bg-green-700': estado === 'Disponible'
+    }"
+    >
+        {{ vehicule }}
+        {{ estado }}
+        ${{ price }}
+    </button>
+</template>
